e15d2fbdb6184d945841a3f65b20a6ca169c302a
Oskar Harenda <oskarharenda@gmail.com>
2020-09-06 09:07:22 +0200

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cours';
+  liczba = Math.PI;
+  dog = new Dog('Reksio', 12);
+  date = new Date();
+  days = ['monday', 'thusaday', 'wendsday', 'thursday', 'friday', 'saturday', 'sunday' ];
+  dogs = new Array<Dog>();
+  constructor(){
+    this.dogs.push(new Dog('reksio', 12), new Dog('atos', 2));
+  }
+  showDog(){
+    return('Imię psa: ' + this.dog.name);
+  }
+}
+
+class Dog {
+  constructor(public name: string, public age: number){
+  }
 }
